c185aa1c27bda678579c45b9e951772204317413
Prajwal <prajwaldhule36@gmail.com>
2025-04-26 17:16:35 +0530

    main: added port for fastapi on production

--- main.py
+++ main.py
@@ -26,3 +26,7 @@ def get_news():
     print("news: ", news_with_translations)
     
     return news_with_translations
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Get PORT from env, default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=False)
